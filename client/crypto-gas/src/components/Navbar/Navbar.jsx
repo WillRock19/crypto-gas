574d94d01a91fe6a6dc930ed7477060ec5bba77a
WillRock19 <williamporto_14@hotmail.com>
2022-01-29 19:17:23 -0300

    Making nav menu and adding first styles to it

---
+++ client/crypto-gas/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import logo from '../../images/logo.png';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div id="logo">
+                <img alt="crypt-gas-logo" src={logo} />
+            </div>
+            <div id="menu">
+                <ul>
+                    <li className="menu-item">
+                        <a>Gas fees</a>
+                    </li>
+                    <li className="menu-item">
+                        <a>About us</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;

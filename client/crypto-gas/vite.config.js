f49775bc32d48d9564949d70251609ff0956fa4a
Sérgio Prates dos Santos <sergio.prates@lambda3.com.br>
2022-02-09 19:57:40 -0300

    wip

--- client/crypto-gas/vite.config.js
+++ client/crypto-gas/vite.config.js
@@ -5,12 +5,6 @@ const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '/src'),
-      '~bootstrap': 'bootstrap'
-    }
-  },
   css: {
     preprocessorOptions: {
       scss: {

139bf7f768fcfcf3c6315b0afb4e083d0aea82a1
William Pinto <williamporto_14@hotmail.com>
2022-04-10 22:47:49 -0300

    Removido contrato ABI desnecessário

--- client/crypto-gas/src/services/web3.js
+++ client/crypto-gas/src/services/web3.js
@@ -5,7 +5,7 @@ const infuraEndpoint = import.meta.env.VITE_ETHEREUM_NETWORK_ENDPOINT;
 const web3 = new Web3(infuraEndpoint);
 
 const obterEthEmDolar = () => {
-  const contractData = contracts.find((contract) => contract.name === 'eth-usd-2').data;
+  const contractData = contracts.find((contract) => contract.name === 'eth-usd').data;
   const contract = new web3.eth.Contract(contractData.ABI, contractData.address);
 
   return contract.methods.latestAnswer().call();
